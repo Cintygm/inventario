557fc4757da601e8783db89d205beb9083eb6e95
Cintygm <169121491+Cintygm@users.noreply.github.com>
2026-09-06 19:25:20 -0500

    Crear el pie de Pagina del sistema
    Crear el pie de Pagina del sistema

---
+++ views/partials/footer.php
@@ -0,0 +1,11 @@
+  <footer class="text-center">
+    <div class="container">
+      <hr>
+      Sistema de Inventario Básico &mdash; Proyecto by Cinthia Guaman PHP + MySQL + MVC
+    </div>
+  </footer>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
